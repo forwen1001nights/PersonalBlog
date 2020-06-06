d9abbb56cf184ed40c2242936abdf2b4d457a081
bobby <981277642@qq.com>
2020-06-07 00:51:54 +0800

    change title and ico

--- server-koa/app.js
+++ server-koa/app.js
@@ -1,11 +1,12 @@
 const syncAll = require('./models/sync');
-syncAll();
+const path = require('path');
 require('./service/init');
 const koa = require("koa2");
 const serverConfig = require("./config/server");
+const { historyApiFallback }= require('koa2-connect-history-api-fallback');
 const app = new koa();
 app.proxy = true;
-
+syncAll();
 // middlewares
 const bodyParser = require('koa-bodyparser');
 const cors = require('./middlewares/cors');
@@ -15,8 +16,13 @@ const static = require("koa-static");
 // routers
 const adminRouter = require("./routes/admin");
 const blogRouter = require("./routes/blog");
-
-app.use(static(__dirname + '/public'))
+// app.use(async (req,res) => {
+//     console.log(req)
+// })
+app.use(historyApiFallback({
+    htmlAcceptHeaders: ['text/html', '*/*']
+}));
+app.use(static(__dirname + '/public'));
 app.use(cors);
 app.use(bodyParser());
 
